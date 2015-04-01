fea1a1ab6a855919e31121b3ba2cfbd89c80468b
Louis Tur <louis.tur@gmail.com>
2015-04-01 00:30:44 -0400

    Begins to add chat tableview

---
+++ SRCatChatTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SRCatChatTableViewController.h
+//  SRFirebaseScratchPad
+//
+//  Created by Louis Tur on 4/1/15.
+//  Copyright (c) 2015 Louis Tur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SRCatChatTableViewController : UITableViewController
+
+@end
